d260b024d6de483a566401901bc9993fd6d9d851
UIXER <evgeniu.ermolenko@gmail.com>
2023-10-11 23:48:00 +0300

    Added middlewares

--- server/routes/roleRouter.js
+++ server/routes/roleRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const roleRouter = new Router()
 const RoleController = require('../controllers/RoleController')
+const RoleMiddleware = require('../middleware/RoleMiddleware')
 
 roleRouter.get('/', RoleController.getAll)
-roleRouter.post('/create', RoleController.create)
+roleRouter.post('/create', RoleMiddleware(3), RoleController.create)
 
 module.exports = roleRouter
